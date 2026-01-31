2bfbf59b4bab8fc031f207908a4039a4343ef4d4
McR <mathieu.c.roy@gmail.com>
2026-01-31 12:10:39 -0500

    Partial background color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,9 +5,16 @@ return {
     "nickkadutskyi/jb.nvim",
     priority = 1000,
     init = function()
-      vim.api.nvim_create_autocmd("VimEnter", {
+      local bg = "#11131A"
+      vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
         callback = function()
-          vim.api.nvim_set_hl(0, "Normal", { bg = "#11131A" })
+          vim.api.nvim_set_hl(0, "Normal", { bg = bg })
+          vim.api.nvim_set_hl(0, "NormalNC", { bg = bg })
+          vim.api.nvim_set_hl(0, "NormalFloat", { bg = bg })
+          vim.api.nvim_set_hl(0, "FloatBorder", { bg = bg })
+          vim.api.nvim_set_hl(0, "Pmenu", { bg = bg })
+          vim.api.nvim_set_hl(0, "SignColumn", { bg = bg })
+          vim.api.nvim_set_hl(0, "LineNr", { bg = bg })
         end,
       })
     end,
